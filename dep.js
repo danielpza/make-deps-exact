7c1aa26e0e41e29c82cf220c6782a4f8b9f573f9
Daniel Perez Alvarez <danielpza@protonmail.com>
2023-01-05 16:34:25 -0500

    feat: add --dry option

--- dep.js
+++ dep.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 
 import { readFile, writeFile } from "node:fs/promises";
+import { parseArgs } from "node:util";
 
 const pkg = JSON.parse(await readFile("package.json", "utf8"));
 const pkgLock = JSON.parse(await readFile("package-lock.json", "utf8"));
+
+const { values, positionals } = parseArgs({
+  args: process.argv.slice(2),
+  options: {
+    "skip-git": {
+      type: "boolean",
+    },
+    dry: {
+      type: "boolean",
+      short: "d",
+    },
+  },
+});
+
+const { dry } = values;
 
 const modes = ["dependencies", "devDependencies", "optionalDependencies"];
 
@@ -13,11 +29,10 @@ for (const mode of modes) {
     const version = pkg[mode][key];
     const actual = pkgLock.dependencies[key].version;
     if (version !== actual) {
-      // if (!version.match(actual)) {
       console.log(`changing ${key} from ${version} to ${actual}`);
       pkg[mode][key] = actual;
     }
   }
 }
 
-await writeFile("package.json", JSON.stringify(pkg, null, 2));
+if (!dry) await writeFile("package.json", JSON.stringify(pkg, null, 2));
